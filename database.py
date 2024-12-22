d13564c962ed995f0014161fdf43ceef87acd4a4
O'tkir <142495809+adm1ngit@users.noreply.github.com>
2024-12-22 15:41:05 +0500

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# Bazani sozlash
-DATABASE_URL = "postgresql://postgres:IyemKEneTFbrBaGOSHTtLsHrKUGvjagt@autorack.proxy.rlwy.net:47798/railway"
-Base = declarative_base()
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
